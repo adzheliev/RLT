712b6c9bd69b1471d4b79a4d511174b6349494eb
Alan Dzheliev <109296176+adzheliev@users.noreply.github.com>
2023-11-11 12:58:15 +0300

    Delete tokens.py

--- tokens.py
+++
@@ -1,5 +0,0 @@
-"""Module to store tokens and passwords"""
-
-mongoDB_token: str = 'djalan84'
-mongoDB_password: str = 'Demid2019Mark2022'
-bot_token: str = '6673316358:AAFQkLmdbSmrYPtLEfNUgymjnY-VspWBL3o'
